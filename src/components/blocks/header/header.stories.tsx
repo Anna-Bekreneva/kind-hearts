bf597989eb6bb0f10156269c05c21bb553360ae4
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-04-19 16:56:00 +0300

    add header stories

---
+++ src/components/blocks/header/header.stories.tsx
@@ -0,0 +1,14 @@
+import { Header } from '@/components'
+import { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-remix-react-router'
+
+const meta = {
+  component: Header,
+  decorators: [withRouter],
+  tags: ['autodocs'],
+  title: 'Components/Blocks/Header',
+} satisfies Meta<typeof Header>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const HeaderDefault: Story = {}
