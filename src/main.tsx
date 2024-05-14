32cc6eec89be1c9ff1f7fb0479335f06213bdf33
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-14 13:08:16 +0300

    add emailJs, add toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import { StrictMode } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import { App } from '@/App'
 import { createRoot } from 'react-dom/client'
 
 import './styles/index.scss'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<App />} path={'/'} />
+        <Route
+          element={
+            <>
+              <App />
+              <ToastContainer autoClose={50000} position={'bottom-right'} />
+            </>
+          }
+          path={'/'}
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
